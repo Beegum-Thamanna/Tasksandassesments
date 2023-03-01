0a5016965c042091afa34c069a29e714169e5192
Preethesh K <Preethesh.K@chordify.com>
2023-03-01 16:52:53 +0530

    Ruby_Basic

---
+++ test.rb
@@ -0,0 +1,8 @@
+
+puts "enter a number:"
+i = Integer(gets.chomp)
+if i % 2 == 0
+   puts "even"
+else
+   puts "odd"
+end
---
+++ test3.rb
@@ -0,0 +1,16 @@
+string1 = gets.chomp
+
+puts "entered string is #{string1}!"
+
+lgt= string1.length
+
+puts "Number of characters are #{string1.length}"
+
+
+if lgt > 8
+	puts "print eligible for password creation"
+
+else
+	puts "not eligibile"
+
+end
